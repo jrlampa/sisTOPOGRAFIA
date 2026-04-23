24b823e6d0d6a5f5b7ad39b78b5d67d74d1de9a5
Jonatas Lampa <jonatas.lampa@im3.com.br>
2026-04-22 22:21:58 -0300

    fix(cors): add localhost:3001 to allowed origins for dev environment

--- sisrua_unified/server/app.ts
+++ sisrua_unified/server/app.ts
@@ -153,6 +153,8 @@ if (isProduction) {
 } else {
   allowedOrigins = [
     "http://localhost:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
     "http://localhost:5173",
     "http://localhost:4173",
     "http://127.0.0.1:5173",
